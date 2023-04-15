38fe83c053634013f5acf9660dbfe525e7654b63
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-04-14 22:36:24 -0500

    04/15/2023 Daily

---
+++ Leetcode/Leetcode Daily's/2218. Maximum Value of K Coins From Piles/2218-Maximum-Value-of-K-Coins-From-Piles.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
+        n = len(piles)
+
+        dp = [[-1]*(k+1) for i in range(n) ]
+
+        def dfs(i,c):
+            if i == n:
+                return 0
+            
+            if dp[i][c] != -1:
+                return dp[i][c]
+
+            dp[i][c] = dfs(i+1,c) #skip pile
+            cur = 0
+
+            for j in range(min(len(piles[i]),c)):
+                cur += piles[i][j]
+                dp[i][c] = max(cur + dfs(i + 1, c - j - 1), dp[i][c])
+            
+            return dp[i][c]
+
+
+        return dfs(0,k)
