c21e27e294e50212299333b1c5c6463e80c260ef
Ayo Osonowo <46603881+ayooo1@users.noreply.github.com>
2023-05-20 09:49:53 -0500

    Added js 30 day challenge and finished day 16 and the daily for 05/20/2023

--- Leetcode/Leetcode Daily's/399. Evaluate Division/399-Evaluate-Division-bfs.py
+++ Leetcode/Leetcode Daily's/399. Evaluate Division/399-Evaluate-Division-bfs.py
@@ -1,48 +1,42 @@
+from collections import defaultdict, deque
+
+
 class Solution:
     def calcEquation(self, equations: list[list[str]], values: list[float], queries: list[list[str]]) -> list[float]:
-        def find(x):
-            if x == root[x]:
-                return root[x], vals[x]
+        d = defaultdict(list)
 
-            root[x], val = find(root[x])
-            vals[x] = vals[x] * val
+        for i, (u,v) in enumerate(equations):
+            d[u].append((v,values[i]))
+            d[v].append((u, 1/values[i]))
+        
+        
+        ans = []
 
-            return root[x], vals[x]
 
-        def union(x, y, original_weight):
-            root_x, weight_x = find(x)
-            root_y, weight_y = find(y)
-            if root_x != root_y:
-                if rank[root_x] > rank[root_y]: 
-                    root[root_y] = root_x
-                    vals[root_y] = 1.0 / original_weight * weight_x / weight_y
-                elif rank[root_x] < rank[root_y]:
-                    root[root_x] = root_y
-                    vals[root_x] = original_weight * weight_y / weight_x
+        for s,e in queries:
+            if s == e:
+                if s not in d:
+                    ans.append(float(-1))
+                    continue
                 else:
-                    root[root_x] = root_y
-                    vals[root_x] = original_weight * weight_y / weight_x
-                    rank[root_y] = rank[root_y] + 1
+                    ans.append(float(1))
 
-        # sum(a_2D_list, []) will flaten the 2D list. We then convert it to a set.
-        nodes = set(sum(equations, []))
-        root = {c: c for c in nodes}
-        vals = {c: 1 for c in nodes}
-        rank = {c: 1 for c in nodes}
-
-        for (a, b), v in zip(equations, values):
-            union(a, b, v)
-
-        result = []
-        for c, d in queries:
-            if c in nodes and d in nodes: 
-                parent_c, weight_c = find(c)
-                parent_d, weight_d = find(d)
-                if parent_c == parent_d:
-                    result.append(weight_c / weight_d)
-                else:
-                    result.append(-1)
             else:
-                result.append(-1)
+                q = deque([(s,1)])
+                seen = set()
+                hasAnswer = False
 
-        return result
+                while q:
+                    cur,val = q.popleft()
+                    if cur == e:
+                        ans.append(val)
+                        hasAnswer = True
+                    
+                    for node,v in d[cur]:
+                        if node not in seen:
+                            q.append((node,val*v))
+                            seen.add(node)
+                if not hasAnswer:
+                    ans.append(float(-1))
+        
+        return ans
